5a35728f04d1036bf8783298f51e9672fa1146ef
Giovanna Marques Freire Barbosa <giomfbarbosa@gmail.com>
2025-06-10 09:43:48 -0300

    Add files via upload

--- Music-swift/Music-swift/FadeViewController.swift
+++ Music-swift/Music-swift/FadeViewController.swift
@@ -9,15 +9,21 @@ import UIKit
 
 class FadeViewController: UIViewController {
 
-   
+    @IBOutlet weak var slider: UISlider!
+    var counter = 0
+    
     override func viewDidAppear(_ animated: Bool){
-        view.backgroundColor = .white
-        var counter = 5.0
-        let timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true){ timer in counter-=1}
-        let alpha = 0.2*counter
-        self.view.backgroundColor = .black
-        if counter > 5 {timer.invalidate()}
+        slider.maximumValue = 100.0
+      
+        let timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true){ timer in self.counter+=1
+            self.slider.value = Float(self.counter)
+            
+            
+        }
+        
+        if counter > 100 {timer.invalidate()}
     }
 
 
+    
 }
